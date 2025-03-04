4da9d94bbd2eb1cf1a5eac039540fd09e862c03d
jayshah41 <k23006016@kcl.ac.uk>
2025-03-04 23:36:40 +0000

    Add styling and structure to the Contact component with two-card layout to reflect how it is done on the MakCorp website

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactCard from './ContactCard';
+import './GeneralStyles.css';
 import steveRosewell from '../assets/MeetTheTeam/steve-rosewell.png';
 import emmanuelHeyndrickx from '../assets/MeetTheTeam/emmanuel-heyndrickx.png';
 import scottYull from '../assets/MeetTheTeam/scott-yull.png';
@@ -8,10 +9,14 @@ import robertWilliamson from '../assets/MeetTheTeam/robert-williamson.png';
 const Contact = () => {
   return (
     <>
+    <div className='two-card-container'>
       <ContactCard image={steveRosewell} name={'Steve Rosewell'} role={'Executive Chairman'} phone={'+61 (4) 0555 1055'} email={'steve@makcorp.com.au'} facebook={''} linkedin={''} twitter={''} />
       <ContactCard image={robertWilliamson} name={'Robert Williamson'} role={'Director'} phone={''} email={'robert@makcorp.com.au'} facebook={''} linkedin={''} twitter={''} />
+    </div>
+    <div className='two-card-container'>
       <ContactCard image={scottYull} name={'Scott Yull'} role={'Director'} phone={''} email={'info@makcorp.com.au'} facebook={''} linkedin={''} twitter={''} />
       <ContactCard image={emmanuelHeyndrickx} name={'Emmanuel Heyndrickx'} role={'Executive Director'} phone={'+44 7739 079 787'} email={'emmanuel@makcorp.com.au'} facebook={''} linkedin={''} twitter={''} />
+    </div>
     </>
   )
 }
