809aba81a7144d8c8ba915fc5df06981dab8b30b
jayshah41 <k23006016@kcl.ac.uk>
2025-03-04 23:18:47 +0000

    Refactor in line with css changes

--- src/components/ServicesCardContainer.jsx
+++ src/components/ServicesCardContainer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { FaDollarSign, FaChartLine, FaDatabase } from 'react-icons/fa';
 import ServicesCard from './ServicesCard';
-import './ServicesCardContainer.css'
+import './GeneralStyles.css';
 
 const ServicesCardContainer = () => {
   return (
-    <div className="container">
+    <div className="three-card-container">
         <ServicesCard icon={FaDollarSign} title="Commodity Pricing" content="See the prices for each commodity on a daily basis including potential value of JORCS."/>
         <ServicesCard icon={FaChartLine} title="Stock Performance" content="See the performances on stocks by any period since 2018 including daily, weekly, monthly, and yearly." />
         <ServicesCard icon={FaDatabase} title="Data Services" content="Contact us for other data services including project research and director research." />
