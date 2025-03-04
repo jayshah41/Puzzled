474bc2a6eb796f3e3f778c149235299a58e196a8
jayshah41 <k23006016@kcl.ac.uk>
2025-03-04 23:40:03 +0000

    Update ContactCard component to adjust layout width and add conditional rendering for phone number

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -4,13 +4,13 @@ import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const ContactCard = ({ image, name, role, phone, email, facebook, linkedin, twitter }) => {
   return (
-    <div style={{ width: '15vw', display:'flex', flexDirection: 'column' }}>
+    <div style={{ width: '25vw', display:'flex', flexDirection: 'column', border:'15px solid black' }}>
         <img src={image}></img>
         <h3>{name}</h3>
         <h3 style={{color: 'grey'}}>{role}</h3>
-        <h4>tel: {phone}</h4>
+        {phone ? <h4>tel: {phone}</h4> : <></>}
         <h4>email: {email}</h4>
-        <div className="mt-12 flex justify-center space-x-6 text-2xl text-blue-600">
+        <div className="three-card-container">
             <FaFacebook className="cursor-pointer hover:text-blue-800" />
             <FaLinkedin className="cursor-pointer hover:text-blue-800" />
             <FaTwitter className="cursor-pointer hover:text-blue-800" />
