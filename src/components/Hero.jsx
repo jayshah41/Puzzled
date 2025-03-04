c16ae94158e2a0944a657318f4cb775321e54df7
jayshah41 <k23006016@kcl.ac.uk>
2025-03-04 23:18:11 +0000

    Integrate HeroContainer component into Hero section for improved layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import hero from '../assets/hero-picture.png';
+import HeroContainer from './HeroContainer';
 
 const Hero = () => {
   return (
@@ -7,15 +8,8 @@ const Hero = () => {
       <div>
         <h1>MakCorp has modernised how our clients invest in Mining, Oil & Gas.</h1>
         <p>Compare & analyse ASX resource companies, including</p>
-        <ul>
-          <li>Over 30,000 ASX projects/tenements including commodities, stages, locations, jorcs and more</li>
-          <li>Over 8,500 directors including remuneration and shareholdings</li>
-          <li>Over 2,700 capital raises and their information</li>
-          <li>Over 29,000 Top 20 shareholders transactions</li>
-          <li>Financials including quarterlies, half yearly and annual</li>
-        </ul>
+        <HeroContainer />
         <button>Start now</button>
-        <img src={hero}></img>
       </div>
     </section>
   );
