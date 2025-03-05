5a01de83aedce23fe7a89194db30c036a6baebfb
jayshah41 <k23006016@kcl.ac.uk>
2025-03-05 14:58:46 +0000

    Make it so that the actual links can be passed to the 'Socials' component optionally

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 import './GeneralStyles.css';
 
-const Socials = () => {
+const Socials = ({ facebook="https://facebook.com", linkedin="https://linkedin.com", twitter="https://twitter.com" }) => {
   return (
     <div className="social">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+        <a href={facebook} target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+        <a href={linkedin} target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+        <a href={twitter} target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
     </div>
   )
 }
