f3ebeee4e33ba485a5cbd134e8518904859255e7
jayshah41 <k23006016@kcl.ac.uk>
2025-03-06 18:18:17 +0000

    Refactor ContactCard component for improved styling and layout

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './GeneralStyles.css';
-import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 import Socials from './Socials';
 
 const ContactCard = ({ image, name, role, phone, email, facebook, linkedin, twitter }) => {
   return (
-    <div style={{ width: '25vw', display:'flex', flexDirection: 'column', border:'15px solid black' }}>
+    <div style={{ width: '25vw', backgroundColor: 'white', display:'flex', flexDirection: 'column', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', margin: 'auto' }}>
         <img src={image}></img>
-        <h3>{name}</h3>
-        <h3 style={{color: 'grey'}}>{role}</h3>
-        {phone ? <h4>tel: {phone}</h4> : <></>}
-        {email ? <h4>email: {email}</h4> : <></>}
+        <h3 style={{ margin: 'auto' }}>{name}</h3>
+        <h3 style={{color: 'grey', margin: 'auto'}}>{role}</h3>
+        {phone ? <h4 style={{ margin: 'auto' }}>tel: {phone}</h4> : <></>}
+        {email ? <h4 style={{ margin: 'auto' }}>email: {email}</h4> : <></>}
         <Socials />
 
     </div>
