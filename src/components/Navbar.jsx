c81309228ee5634e8c6ea5ed91b6f270d9ffec1c
jayshah41 <k23006016@kcl.ac.uk>
2025-03-07 19:16:38 +0000

    Implement login and sign up buttons - both go to different elements of the same 'Login' component so that the design reflects the MakCorp website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import makcorpLogoWithText from '../assets/makcorpLogoWithText.png';
 import '../styles/Navbar.css';
 import { Link } from 'react-router-dom';
+import Login from './Login';
 
 const Navbar = () => {
+  
+  const [showingLogin, setShowingLogin] = useState(false);
+  const [showingSignup, setShowingSignup] = useState(false);
+
   return (
     <nav className="navbar sticky">
       <div className="navbar-container">
-      <div className="flex items-center">
-        <Link to="/"><img src={makcorpLogoWithText} alt="MakCorp Logo" height="80px" style={{ padding: '10px' }} /></Link>
-      </div>
+        <div className="flex items-center">
+          <Link to="/"><img src={makcorpLogoWithText} alt="MakCorp Logo" height="80px" style={{ padding: '10px' }} /></Link>
+        </div>
 
         <Link to="/">Home</Link>
         <Link to="/pricing">Pricing</Link>
         <Link to="/products">Products</Link>
         <Link to="/contact-us">Contact us</Link>
 
-
-      <div>
-        <button>Log In</button>
-        <button>Sign Up</button>
+        <div>
+          <button onClick={() => {setShowingLogin(true); setShowingSignup(false);}}>Log In</button>
+          <button onClick={() => {setShowingSignup(true); setShowingLogin(true);}}>Sign Up</button>
+        </div>
       </div>
 
-      </div>
+      {showingLogin && <Login onClose={() => setShowingLogin(false)} loginButton={!showingSignup}/>}
     </nav>
   );
 };
