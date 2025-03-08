51118601da51dfa15ae45d426636b8cc7e8483c6
jayshah41 <k23006016@kcl.ac.uk>
2025-03-08 15:41:34 +0000

    Add PricingHero component with subscription details and header image

---
+++ src/components/PricingHero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import pricingHeaderImage from '../assets/pricing-header-image.png';
+import '../styles/GeneralStyles.css';
+
+const PricingHero = () => {
+  return (
+    <div className="two-card-container standard-padding">
+        <div>
+        <h1>MakCorp Platform through Affordable Subscriptions</h1>
+        <p>The MakCorp platform provides our users with access to 6 key data modules with over 600 data points to provide our clients with the ability to make better informed investment decisions.
+        As an example, using projects data, users can seamlessly filter based upon key indicators like commodity type, geographic location or project stage to identify potential investment or client oppotunities.</p>
+        <button className="defulatButton">Start now</button>
+        </div>
+        <img src={pricingHeaderImage} style={{ width: '45vw', paddingLeft: "35px" }}></img>
+    </div>
+  )
+}
+
+export default PricingHero
