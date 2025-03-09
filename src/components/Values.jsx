bf0b37636fd72d074001523963ef6ed9785ffa9b
jayshah41 <k23006016@kcl.ac.uk>
2025-03-09 18:38:36 +0000

    Add css styling to Values component

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import '../styles/ValueComponent.css';
 import ValueComponent from './ValueComponent';
 
 const Values = () => {
   return (
-    <div>
-        <ValueComponent />
-        <ValueComponent />
-        <ValueComponent />
-        <ValueComponent />
+    <div className="values-container">
+      <div className="values-header">
+        <h1>MakCorp's Value to Clients</h1>
+      </div>
+      
+      <div className="values-list">
+        <ValueComponent index={1} />
+        <ValueComponent index={2} />
+        <ValueComponent index={3} />
+        <ValueComponent index={4} />
+      </div>
     </div>
   )
 }
