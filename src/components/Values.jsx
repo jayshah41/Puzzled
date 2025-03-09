50174172466bca112da2354f305164df2f435aa6
jayshah41 <k23006016@kcl.ac.uk>
2025-03-09 17:25:10 +0000

    Basic Values component

---
+++ src/components/Values.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ValueComponent from './ValueComponent';
+
+const Values = () => {
+  return (
+    <div>
+        <ValueComponent />
+        <ValueComponent />
+        <ValueComponent />
+        <ValueComponent />
+    </div>
+  )
+}
+
+export default Values
