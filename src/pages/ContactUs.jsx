ab6d7f61267110ba15b83d7ef6444ff2cb708dc2
lancelottso <lanceericcastroso@gmail.com>
2025-03-09 22:46:55 +0000

    Finished Contact Us Form

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ContactUsHero from '../components/ContactUsHero';
+import ContactUsForm from '../components/ContactUsForm';
 
 const ContactUs = () => {
   return (
     <>
       <ContactUsHero/>
+      <ContactUsForm/>
     </>
   )
 }
