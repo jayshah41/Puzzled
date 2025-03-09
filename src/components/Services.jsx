c04c070811a17cf3d1b267a6670d8bdbdd835a01
jayshah41 <k23006016@kcl.ac.uk>
2025-03-09 18:50:22 +0000

    Improved formatting of Services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,15 +5,15 @@ import Socials from './Socials';
 
 const Services = () => {
   return (
-    <div className="standard-padding">
-      <h2 className="text-4xl centre">Services we provide</h2>
-      <div style={{ width: '70vw', margin: 'auto' }}>
+    <div className="standard-padding" style={{ textAlign: 'center' }}>
+      <h1> Services we provide</h1>
+      <div style={{ width: '70vw', margin: 'auto', marginBottom: '75px' }}>
         <p className="text-gray-600 centre">
           Makcorp provides a wide range of services for opportunities related to the mining industry.
           Whether you are an investor or a business looking to expand your footprint within the industry,
           MakCorp has tools available to provide research and analytics on mining organisations listed on the ASX.
         </p>
-        <p className="text-gray-600 centre">
+        <p className="text-gray-600 centre" style={{ marginBottom: '75px' }}>
           The MakCorp platform can help you become more successful whether you are a retail investor,
           a corporate investor, or a business owner. Let us help you find your next opportunity for growth.
         </p>
