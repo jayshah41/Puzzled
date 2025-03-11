b4be1aa34e496ee5e97fb0a0da4089f4bcf23a53
lancelottso <lanceericcastroso@gmail.com>
2025-03-11 16:07:00 +0000

    Finished products page

---
+++ src/components/ProductsJoin.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/GeneralStyles.css';
+
+const ProductsJoin = () => {
+  return (
+    <div style={{ margin: "75px" }}>
+      <div className="flex flex-col items-center justify-center text-center">
+        <h1 className="centre">Join The MakCorp Community</h1>
+        <div className="centre">
+        <button className="defulatButton">Start now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsJoin;
