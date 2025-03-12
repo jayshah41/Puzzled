62abdd9edc8a709887c81f8131b94dc0a63f982b
Camille Junique (k23057058) <camille.junique@kcl.ac.uk>
2025-03-12 18:55:32 +0000

    new files for graphs

---
+++ src/pages/Graph.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+//import ... from components/CompanyDetails
+//import ... from components/MarketData
+//import ... from components/MarketTrends
+//import ... from components/Directors
+//import ... from components/Shareholders
+//import ... from components/CapitalRaises
+//import ... from components/Projects
+//import ... from components/Financials
+
+
+
+
+
