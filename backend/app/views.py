fb415191fa41285c0a32d3bb5d9a0904525d7ff3
jayshah41 <k23006016@kcl.ac.uk>
2025-03-13 18:04:21 +0000

    Add EditableContent views for listing and updating content

--- backend/app/views.py
+++ backend/app/views.py
@@ -6,6 +6,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
+from .models import EditableContent
+from .serializers import EditableContentSerializer
 
 User = get_user_model()
 
@@ -41,3 +43,24 @@ class ProfileView(generics.RetrieveAPIView):
     def get_object(self):
         return self.request.user  # Returns the logged-in user's data
 
+
+class EditableContentView(generics.ListAPIView):
+    serializer_class = EditableContentSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        component = self.request.query_params.get('component', None)
+        if component:
+            return EditableContent.objects.filter(component=component)
+        return EditableContent.objects.all()
+
+
+class EditableContentUpdateView(generics.UpdateAPIView):
+    queryset = EditableContent.objects.all()
+    serializer_class = EditableContentSerializer
+    permission_classes = [AllowAny]
+
+    def get_object(self):
+        component = self.request.data.get('component')
+        section = self.request.data.get('section')
+        return EditableContent.objects.get(component=component, section=section)
