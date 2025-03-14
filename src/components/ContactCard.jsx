765ef776959e77c25f90a2a50320eb15c610d408
jayshah41 <k23006016@kcl.ac.uk>
2025-03-14 00:00:26 +0000

    Implement editable contact information functionality in Contact and ContactCard components

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -2,18 +2,61 @@ import React from 'react';
 import '../styles/GeneralStyles.css';
 import Socials from './Socials';
 
-const ContactCard = ({ image, name, role, phone, email, facebook, linkedin, twitter }) => {
+const ContactCard = ({ contact, index, setContacts, isEditing }) => {
+  const handleChange = (field, value) => {
+    setContacts(prevContacts => {
+      const updatedContacts = [...prevContacts];
+      updatedContacts[index][field] = value;
+      return updatedContacts;
+    });
+  };
+
   return (
     <div style={{ width: '25vw', backgroundColor: 'white', display:'flex', flexDirection: 'column', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', margin: 'auto' }}>
-        <img src={image}></img>
-        <h3 style={{ margin: 'auto' }}>{name}</h3>
-        <h3 style={{color: 'grey', margin: 'auto'}}>{role}</h3>
-        {phone ? <h4 style={{ margin: 'auto' }}>tel: {phone}</h4> : <></>}
-        {email ? <h4 style={{ margin: 'auto' }}>email: {email}</h4> : <></>}
+        <img src={contact.image} alt={contact.name} />
+        {isEditing ? (
+          <input
+            type="text"
+            value={contact.name}
+            onChange={(e) => handleChange('name', e.target.value)}
+            className="auth-input"
+          />
+        ) : (
+          <h3 style={{ margin: 'auto' }}>{contact.name}</h3>
+        )}
+        {isEditing ? (
+          <input
+            type="text"
+            value={contact.role}
+            onChange={(e) => handleChange('role', e.target.value)}
+            className="auth-input"
+          />
+        ) : (
+          <h3 style={{color: 'grey', margin: 'auto'}}>{contact.role}</h3>
+        )}
+        {isEditing ? (
+          <input
+            type="text"
+            value={contact.phone}
+            onChange={(e) => handleChange('phone', e.target.value)}
+            className="auth-input"
+          />
+        ) : (
+          contact.phone ? <h4 style={{ margin: 'auto' }}>tel: {contact.phone}</h4> : <></>
+        )}
+        {isEditing ? (
+          <input
+            type="text"
+            value={contact.email}
+            onChange={(e) => handleChange('email', e.target.value)}
+            className="auth-input"
+          />
+        ) : (
+          contact.email ? <h4 style={{ margin: 'auto' }}>email: {contact.email}</h4> : <></>
+        )}
         <Socials />
-
     </div>
   )
 }
 
-export default ContactCard
+export default ContactCard;
