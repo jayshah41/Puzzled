664cac445a2345298d360cc7e02687b68f7c9a71
jayshah41 <k23006016@kcl.ac.uk>
2025-03-16 23:06:26 +0000

    Add 'content' app to INSTALLED_APPS in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
-
-    'app',
     'api',
+    'app',
+    'content',
     'rest_framework',
     'corsheaders'
 ]
