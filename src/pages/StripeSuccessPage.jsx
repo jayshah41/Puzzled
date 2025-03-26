df3a13790d5c360a4a68830d7292c5000dae919f
jayshah41 <k23006016@kcl.ac.uk>
2025-03-26 00:35:26 +0000

    Update project configuration and improve StripeSuccessPage styling

--- src/pages/StripeSuccessPage.jsx
+++ src/pages/StripeSuccessPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/GeneralStyles.css';
 
 const StripeSuccessPage = () => {
   const navigate = useNavigate();
@@ -45,7 +46,7 @@ const StripeSuccessPage = () => {
   }, [navigate]);
 
   return (
-    <div>
+    <div className="standard-padding">
       <h2>Subscription Successful!</h2>
       <h2>{statusMessage}</h2>
 
